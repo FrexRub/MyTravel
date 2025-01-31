d5b5d7da5ed314205c5d90d01a0dc73c7a30bf86
Станислав Рубцов <frexrub@gmail.com>
2025-01-31 11:16:16 +0300

    edit BaseDAO

--- app/core/dao.py
+++ app/core/dao.py
@@ -11,7 +11,7 @@ class BaseDAO:
     model = None
 
     @classmethod
-    async def find_one_or_none_by_id(cls, session: AsyncSession, data_id: int):
+    async def find_one_or_none_by_id(cls, data_id: int):
         """
         Асинхронно находит и возвращает один экземпляр модели по указанным критериям или None.
 
@@ -21,13 +21,13 @@ class BaseDAO:
         Возвращает:
             Экземпляр модели или None, если ничего не найдено.
         """
-        # async with async_session_maker() as session:
-        query = select(cls.model).filter_by(id=data_id)
-        result = await session.execute(query)
-        return result.scalar_one_or_none()
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(id=data_id)
+            result = await session.execute(query)
+            return result.scalar_one_or_none()
 
     @classmethod
-    async def find_one_or_none(cls, session: AsyncSession, **filter_by):
+    async def find_one_or_none(cls, **filter_by):
         """
         Асинхронно находит и возвращает один экземпляр модели по указанным критериям или None.
 
@@ -37,10 +37,10 @@ class BaseDAO:
         Возвращает:
             Экземпляр модели или None, если ничего не найдено.
         """
-        # async with async_session_maker() as session:
-        query = select(cls.model).filter_by(**filter_by)
-        result = await session.execute(query)
-        return result.scalar_one_or_none()
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(**filter_by)
+            result = await session.execute(query)
+            return result.scalar_one_or_none()
 
     @classmethod
     async def find_all(cls, session: AsyncSession, **filter_by):
@@ -53,13 +53,13 @@ class BaseDAO:
         Возвращает:
             Список экземпляров модели.
         """
-        # async with async_session_maker() as session:
-        query = select(cls.model).filter_by(**filter_by)
-        result = await session.execute(query)
-        return result.scalars().all()
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(**filter_by)
+            result = await session.execute(query)
+            return result.scalars().all()
 
     @classmethod
-    async def add(cls, session: AsyncSession, **values):
+    async def add(cls, **values):
         """
         Асинхронно создает новый экземпляр модели с указанными значениями.
 
@@ -69,16 +69,16 @@ class BaseDAO:
         Возвращает:
             Созданный экземпляр модели.
         """
-        # async with async_session_maker() as session:
-        async with session.begin():
-            new_instance = cls.model(**values)
-            session.add(new_instance)
-            try:
-                await session.commit()
-            except SQLAlchemyError as e:
-                await session.rollback()
-                raise e
-            return new_instance
+        async with async_session_maker() as session:
+            async with session.begin():
+                new_instance = cls.model(**values)
+                session.add(new_instance)
+                try:
+                    await session.commit()
+                except SQLAlchemyError as e:
+                    await session.rollback()
+                    raise e
+                return new_instance
 
 
 class UserDAO(BaseDAO):
