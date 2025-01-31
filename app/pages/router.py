f33a3c2ba8e5500c934f61b8cd0c693fec8de9b4
Станислав Рубцов <frexrub@gmail.com>
2025-01-31 12:39:07 +0300

    add endpoint city

--- app/pages/router.py
+++ app/pages/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 
 
 router = APIRouter(prefix="", tags=["Фронтенд"])
@@ -13,3 +13,20 @@ async def read_root(request: Request):
     return templates.TemplateResponse(
         "index.html", {"request": request, "title": "Элегантная парикмахерская"}
     )
+
+
+@router.get("/city", response_class=JSONResponse)
+async def get_city(request: Request):
+    url = "https://apidojo-booking-v1.p.rapidapi.com/locations/auto-complete"
+    headers = {
+        "X-RapidAPI-Key": site.api_key.get_secret_value(),
+        "X-RapidAPI-Host": site.host_api
+    }
+    try:
+        text_querystring = {"languagecode": "ru"}
+        text_querystring["text"] = name_city
+
+        response = requests.get(url, headers=headers, params=text_querystring)
+
+        date = json.loads(response.text)
+    return {"result": "ok"}
